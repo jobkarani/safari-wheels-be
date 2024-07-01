1aa8a6eb071683150c7b595407d8c6416fa38aec
jobkarani <jobkarani084j@gmail.com>
2024-07-01 22:08:38 +0300

    feat: installs drf_yasg

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,23 @@
 from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from app import views
+from .views import *
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Onboarding Platform API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=[]
+)
 
 urlpatterns = [
     re_path('login', views.login),
