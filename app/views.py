c7f8b4a6a7e0ca1ec191461eb79045f8e87074bc
jobkarani <jobkarani084j@gmail.com>
2024-08-02 13:48:36 +0300

    fix: review serializer class

--- app/views.py
+++ app/views.py
@@ -163,7 +163,7 @@ def create_review(request):
     if request.method == 'POST':
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
