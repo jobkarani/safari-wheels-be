f5088d8ec3756fb86d27fccf63c80803f1cd6ba1
jobkarani <jobkarani084j@gmail.com>
2024-08-20 08:28:10 +0300

    feat: adds id field on profile serializer

--- app/serializer.py
+++ app/serializer.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    user_type = serializers.CharField(source='profile.user_type')
+    user_type = serializers.CharField(null=True, source='profile.user_type')
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'password', 'profile', 'user_type']
